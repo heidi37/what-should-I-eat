a2fe7ca91aacf0a7f66c19b1cb4db5e9b33e48e9
Heidi Fryzell <hfryzell@gmail.com>
2021-08-06 14:40:24 -0400

    diagnose requiring printing.rb with Max

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "what_should_i_eat/version"
+require_relative "what_should_i_eat/printing" 
+require 'http'
 
 module WhatShouldIEat
   class Error < StandardError; end
   # Your code goes here...
+  result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
+  recipes = JSON.parse(result)
 end
---
+++ lib/what_should_i_eat/printing.rb
@@ -0,0 +1,3 @@
+puts "Eat this:"
+  puts recipes.first['items'].first['label']
+  puts recipes.first['items'].first['url']
